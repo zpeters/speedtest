299a13ab2ab84bb4429f0a40bbd7f6f7d500e28d
Zach Peters <zpeters@gmail.com>
2018-09-09 18:32:45 -0500

    Switched comms_test over to IS

--- internal/pkg/comms/comms_test.go
+++ internal/pkg/comms/comms_test.go
@@ -1,34 +1,47 @@
 package comms
 
 import (
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/matryer/is"
 )
 
 func TestConnect(t *testing.T) {
+	is := is.New(t)
+
 	conn := Connect("speedtest.tec.com:8080")
-	assert.NotNil(t, conn)
+
+	is.True(conn != nil) // conn should be something
 }
 
 func TestSend(t *testing.T) {
+	is := is.New(t)
+
 	conn := Connect("speedtest.tec.com:8080")
 	err := Send(conn, "PING")
-	assert.NoError(t, err)
+
+	is.NoErr(err) // should be able to send something
 }
 
 func TestRecv(t *testing.T) {
+	is := is.New(t)
+
 	conn := Connect("speedtest.tec.com:8080")
 	err := Send(conn, "HI")
-	assert.NoError(t, err)
-	data, err := Recv(conn)
-	assert.NoError(t, err)
-	assert.Contains(t, string(data), "HELLO")
+	data, err2 := Recv(conn)
+
+	is.NoErr(err)                                    // should be able to send
+	is.NoErr(err2)                                   // should be able to recieve
+	is.True(strings.Contains(string(data), "HELLO")) // we should have HELLO back
 }
 
 func TestCommand(t *testing.T) {
+	is := is.New(t)
+
 	conn := Connect("speedtest.tec.com:8080")
 	resp, err := Command(conn, "PING 123")
-	assert.NoError(t, err)
-	assert.Contains(t, string(resp), "PONG")
+
+	is.NoErr(err)                                   // command should return a response
+	is.True(strings.Contains(string(resp), "PONG")) // PING should get a PONG
 }
